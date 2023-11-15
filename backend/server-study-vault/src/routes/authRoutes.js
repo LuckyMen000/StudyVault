ef5d360272e687e10386c5a49b43abb36c82bdad
Ab-BeGa <abdulkhakimbegzat@gmail.com>
2023-11-15 10:05:19 +0600

    логика авторизации и регистрации

--- backend/server-study-vault/src/routes/authRoutes.js
+++ backend/server-study-vault/src/routes/authRoutes.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const authController = require('../controllers/authController')
-
+const bcrypt = require('bcrypt');
 const router = express.Router();
+const prisma = require('../db/prisma')
 
 router.use('/auth',authController)
+
+router.post('/register', authController.register)
+router.post('/login',authController.login)
 
 module.exports = router
