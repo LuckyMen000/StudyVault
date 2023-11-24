1a164dd5143ce7b39c75385ea1ad3398b60f50fe
LuckyMen000 <danil_unturned459@mail.ru>
2023-11-23 19:37:39 -0800

    Новые страницы , обновленный SlideBar

--- frontend/src/Components/Administration/Admin.js
+++ frontend/src/Components/Administration/Admin.js
@@ -1,27 +1,25 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import {
   Box,
   Heading,
   Button,
   Modal,
-  ModalOverlay,
-  ModalContent,
-  ModalHeader,
-  ModalFooter,
-  ModalBody,
-  ModalCloseButton,
   Table,
   Tbody,
   Tr,
-  Td, 
+  Td,
   IconButton,
+  ChakraProvider,
 } from '@chakra-ui/react';
 import { SettingsIcon, ArrowBackIcon } from '@chakra-ui/icons';
-import './admin.css'
+import { FaDatabase } from 'react-icons/fa';
+import Sidebar from '../../Components/Slidebar/slidebar';
+import { Link } from 'react-router-dom';
+import './admin.css';
 
 function Dashboard() {
   const [isCreatingUser, setIsCreatingUser] = useState(false);
-  const [users, setUsers] = useState([]); // Список пользователей
+  const [users, setUsers] = useState([]); 
 
   const openCreateUserModal = () => {
     setIsCreatingUser(true);
@@ -32,75 +30,82 @@ function Dashboard() {
   };
 
   const handleLogout = () => {
-    // Обработчик для выхода
   };
 
-  // ... (остальной код)
 
   return (
-    <Box className="admin-container" textAlign="center" p={4}>
-      <Box width="100%" mx="auto">
-        <Box
-          display="flex"
-          justifyContent="space-between"
-          alignItems="center"
-          mb={4}
-        >
-          <Heading className="admin-heading">Панель управления</Heading>
-          <Box>
-            <IconButton
-              icon={<SettingsIcon />}
-              aria-label="Настройки"
+    <ChakraProvider>
+      <div style={{ display: 'flex', margin: '20px' }}>
+        <Sidebar />
+        <Box className="admin-container" textAlign="center" p={4} width="100%">
+          <Box width="100%" mx="auto">
+            <Box
+              display="flex"
+              justifyContent="space-between"
+              alignItems="center"
+              mb={4}
+            >
+              <Heading className="admin-heading">Панель управления</Heading>
+              <Box>
+                <Link to="/admin/database" className="database-button">
+                  <IconButton
+                    icon={<FaDatabase />}
+                    aria-label="База Данных"
+                    mr={2}
+                    onClick={() => {
+                    }}
+                  />
+                </Link>
+                <Link to="/settings" className="admin-button">
+                  <IconButton
+                    icon={<SettingsIcon />}
+                    aria-label="Настройки"
+                    className="admin-button"
+                    onClick={() => {
+                    }}
+                  />
+                </Link>
+                <IconButton
+                  icon={<ArrowBackIcon />}
+                  aria-label="Выйти"
+                  onClick={handleLogout}
+                  ml={2} 
+                />
+              </Box>
+            </Box>
+            <Button
+              onClick={openCreateUserModal}
               className="admin-button"
-              onClick={() => {
-                // Обработчик для настроек
-              }}
-            />
-            <IconButton
-              icon={<ArrowBackIcon />}
-              aria-label="Выйти"
+              colorScheme="teal"
+              mb={4}
+            >
+              Создать пользователя
+            </Button>
+            <Modal isOpen={isCreatingUser} onClose={closeCreateUserModal}>
+            </Modal>
+            <Table className="admin-table">
+              <Tbody>
+                {users.map((user) => (
+                  <Tr key={user.id}>
+                    <Td>{user.name}</Td>
+                    <Td>{user.email}</Td>
+                  </Tr>
+                ))}
+              </Tbody>
+            </Table>
+            <Button
+              className="admin-logout"
               onClick={handleLogout}
-              ml={2} // Добавлен отступ между кнопками
-            />
+              colorScheme="red"
+              mt={4} 
+            >
+              Выйти
+            </Button>
           </Box>
         </Box>
-        <Button
-          onClick={openCreateUserModal}
-          className="admin-button"
-          colorScheme="teal"
-          mb={4} // Добавлен отступ после кнопки
-        >
-          Создать пользователя
-        </Button>
-        <Modal isOpen={isCreatingUser} onClose={closeCreateUserModal}>
-          {/* ... (модальное окно для создания пользователя) */}
-        </Modal>
-        <Table className="admin-table">
-          <Tbody>
-            {users.map((user) => (
-              <Tr key={user.id}>
-                <Td>{user.name}</Td>
-                <Td>{user.email}</Td>
-                {/* Другие поля пользователя */}
-              </Tr>
-            ))}
-          </Tbody>
-        </Table>
-        <Button
-          className="admin-logout"
-          onClick={handleLogout}
-          colorScheme="red"
-          mt={4} // Добавлен отступ перед кнопкой
-        >
-          Выйти
-        </Button>
-      </Box>
-    </Box>
+      </div>
+    </ChakraProvider>
   );
-  }
-  
-
-  
-
+}
 
 export default Dashboard;
