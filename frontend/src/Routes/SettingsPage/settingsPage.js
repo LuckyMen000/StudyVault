f4e4616437083c381f93c15a184edbfc9b60fdfd
LuckyMen000 <danil_unturned459@mail.ru>
2023-11-25 16:10:21 +0600

    добавил на все активные странциы сайдбар

--- frontend/src/Routes/SettingsPage/settingsPage.js
+++ frontend/src/Routes/SettingsPage/settingsPage.js
@@ -1,31 +1,43 @@
 import React from 'react';
-import { Box, Heading, Divider, Switch, VStack } from '@chakra-ui/react';
+import {
+  Box,
+  Heading,
+  Divider,
+  Switch,
+  VStack,
+  ChakraProvider,
+} from '@chakra-ui/react';
 import { SettingsIcon } from '@chakra-ui/icons';
 import { css } from '@emotion/react';
+import Sidebar from '../../Components/Slidebar/slidebar';
+
+
 const customStyles = css`
-  /* Ваши пользовательские стили */
   font-weight: bold;
   color: blue;
 `;
 
-// И примените их к компоненту
 <Box css={customStyles}>Стилизованный блок</Box>
 
 
 const SettingsPage = () => {
   return (
-    <Box p={4}>
-      <Heading as="h1" mb={4} fontSize="2xl">
-        <SettingsIcon mr={2} />
-        Настройки
-      </Heading>
-      <Divider mb={4} />
-      <VStack spacing={4} align="flex-start">
-        <SettingsItem label="Уведомления" />
-        <SettingsItem label="Темная тема" />
-        {/* Добавьте другие элементы настроек */}
-      </VStack>
-    </Box>
+    <ChakraProvider>
+      <div style={{ display: 'flex', margin: '20px' }}>
+        <Sidebar/>
+        <Box p={4}>
+          <Heading as="h1" mb={4} fontSize="2xl">
+            <SettingsIcon mr={2} />
+            Настройки
+          </Heading>
+          <Divider mb={4} />
+          <VStack spacing={4} align="flex-start">
+            <SettingsItem label="Уведомления" />
+            <SettingsItem label="Темная тема" />
+          </VStack>
+        </Box>
+      </div>
+    </ChakraProvider>
   );
 };
 
@@ -34,8 +46,10 @@ const SettingsItem = ({ label }) => {
     <Box w="100%" display="flex" justifyContent="space-between">
       <Box>{label}</Box>
       <Switch
-        defaultChecked={false} // Установите значение по умолчанию
-        onChange={(e) => console.log(`${label} изменено на ${e.target.checked}`)}
+        defaultChecked={false}
+        onChange={(e) =>
+          console.log(`${label} изменено на ${e.target.checked}`)
+        }
       />
     </Box>
   );
