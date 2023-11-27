06a319d5853641ef33648a6a9188c8f26a7e9124
LuckyMen000 <danil_unturned459@mail.ru>
2023-11-26 22:49:03 -0800

    отредчали мэйн , обновил ридми фронта

--- frontend/src/Components/ProfileBox/profilebox.js
+++ frontend/src/Components/ProfileBox/profilebox.js
@@ -1,22 +1,44 @@
-import React from 'react';
-import { Box, Flex, Avatar, Text, VStack } from '@chakra-ui/react';
+import React, { useState } from 'react';
+import {
+  Box, 
+  Flex, 
+  Avatar, 
+  Text, 
+  VStack,
+  Button 
+} from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
 
 const ProfileComponent = () => {
+  const [showButton, setShowButton] = useState(false);
+
   const user = {
     username: 'JohnDoe',
     fullName: 'John Doe',
     avatarURL: 'https://via.placeholder.com/150', 
-    bio: 'Frontend Developer', 
   };
 
   return (
-    <Box p={4} shadow="md" borderWidth="1px" borderRadius="md" bgColor="white" height="130px">
+    <Box
+      p={4}
+      shadow="md"
+      borderWidth="1px"
+      borderRadius="md"
+      bgColor="white"
+      height="130px"
+      onMouseEnter={() => setShowButton(true)}
+      onMouseLeave={() => setShowButton(false)}
+    >
       <Flex align="center" justify="space-between">
         <Avatar size="lg" src={user.avatarURL} alt={user.username} />
         <VStack ml={4} align="flex-start">
           <Text fontSize="xl" fontWeight="bold">{user.fullName}</Text>
           <Text color="gray.500">@{user.username}</Text>
-          <Text color="gray.600">{user.bio}</Text>
+          {showButton && (
+            <Link to="/profile">           
+             <Button colorScheme='blue'>Профиль</Button>
+            </Link>
+          )}
         </VStack>
       </Flex>
     </Box>
--- frontend/src/Routes/Main/main.js
+++ frontend/src/Routes/Main/main.js
@@ -3,16 +3,22 @@ import { ChakraProvider } from '@chakra-ui/react';
 import Sidebar from '../../Components/Slidebar/slidebar';
 import InputForm from '../../Components/Form/input';
 import Profile from '../../Components/ProfileBox/profilebox';
+import NoteBox from '../../Components/NotesBox/Notes';
 
 const Main = () => {
   return (
     <ChakraProvider>
+      <div style={{ display: 'flex', justifyContent: 'space-between'}}>
       <div style={{ display: 'flex', margin: '20px' }}>
         <Sidebar />
-        <div style={{ display: 'flex', justifyContent: 'space-between', width: '100%' }}>
-          <InputForm />
-          <Profile />
+        <InputForm />
+        <div style={{ display: 'flex', flexDirection: 'column', marginLeft: '920px' }}>
+          <div style={{ marginBottom: '20px' }}>
+            <Profile />
+          </div>
+          <NoteBox />
         </div>
+      </div>
       </div>
     </ChakraProvider>
   );
