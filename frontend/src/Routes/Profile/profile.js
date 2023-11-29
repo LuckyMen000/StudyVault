92dadf57c05584a9ab1414c6bb421f672b7522e3
ArsAsul <arsikasulzhan@gmail.com>
2023-11-29 10:50:47 +0600

    обновили профиль

--- frontend/src/Routes/Profile/profile.js
+++ frontend/src/Routes/Profile/profile.js
@@ -1,129 +1,63 @@
-import React, { useState } from 'react';
-import {
-  Box,
-  Button,
-  Input,
-  FormControl,
-  FormLabel,
-  Avatar,
-  Text,
-  ChakraProvider,
-} from '@chakra-ui/react';
-import Slidebar from '../../Components/Slidebar/slidebar';
+import React from "react";
+import { ChakraProvider, Flex, Text, Avatar, Badge, Button, CSSReset, extendTheme } from "@chakra-ui/react";
+import Gradient from './Gradient.png';
+import Ellipse1 from './Ellipse 1.png';
 
-const ProfilePage = () => {
-  const [isEditing, setIsEditing] = useState(false);
-  const [profileData, setProfileData] = useState({
-    username: 'JohnDoe',
-    email: 'john.doe@example.com',
-    avatar: 'https://via.placeholder.com/150',
-    bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    interests: ['Coding', 'Reading', 'Traveling'],
-  });
+const customTheme = extendTheme({
+  fonts: {
+    body: "Poppins, sans-serif",
+    heading: "Poppins, sans-serif",
+  },
+});
 
-  const toggleEditing = () => {
-    setIsEditing((prevState) => !prevState);
-  };
-
-  const handleFormSubmit = (e) => {
-    e.preventDefault();
-    // Logic for form submission here to update profileData state
-    // For example: setProfileData(updatedProfileData);
-    setIsEditing(false); // Exit edit mode after submission
-  };
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setProfileData({
-      ...profileData,
-      [name]: value,
-    });
-  };
+function App() {
+  const backgroundImage = `url(${Gradient})`;
 
   return (
-    <ChakraProvider>
-      <div style={{ display: 'flex', margin: '20px' }}>
-      <Slidebar />
-      <Box
-            width="100%"
-            padding="40px"
-            textAlign="center"
-            fontFamily="Arial, sans-serif"
-        >
-            <Box
-                width={{ base: '100%', md: '80%', lg: '600px' }}
-                margin="0 auto"
-                padding="20px"
-                borderRadius="20px"
-                backgroundColor="#f2f7f5"
-                boxShadow="0 0 10px rgba(0, 0, 0, 0.1)"
-            >
-                <Avatar size="xl" src={profileData.avatar} alt="Avatar" mb="20px" />
-                <Text fontSize="2xl" fontWeight="bold" mb="20px">
-                    {profileData.username}
-                </Text>
-                <FormControl>
-                    {!isEditing ? (
-                        <>
-                            <FormLabel>
-                                <strong>Username:</strong> {profileData.username}
-                            </FormLabel>
-                            <FormLabel>
-                                <strong>Email:</strong> {profileData.email}
-                            </FormLabel>
-                            <FormLabel>
-                                <strong>Bio:</strong> {profileData.bio}
-                            </FormLabel>
-                            <FormLabel>
-                                <strong>Interests:</strong>{' '}
-                                {profileData.interests.map((interest, index) => (
-                                    <span key={index}>{interest} </span>
-                                ))}
-                            </FormLabel>
-                            <Button onClick={toggleEditing} mb="20px">
-                                Edit Profile
-                            </Button>
-                        </>
-                    ) : (
-                        <form onSubmit={handleFormSubmit}>
-                            <FormControl>
-                                <Input
-                                    type="text"
-                                    name="username"
-                                    value={profileData.username}
-                                    onChange={handleChange}
-                                    placeholder="Username"
-                                    mb="10px"
-                                />
-                                <Input
-                                    type="email"
-                                    name="email"
-                                    value={profileData.email}
-                                    onChange={handleChange}
-                                    placeholder="Email"
-                                    mb="10px"
-                                />
-                                <Input
-                                    type="password"
-                                    name="password"
-                                    value={profileData.email}
-                                    onChange={handleChange}
-                                    placeholder="Email"
-                                    mb="10px"
-                                />
-                                <Button type="submit" colorScheme="blue" mr="2">
-                                    Save
-                                </Button>
-                                <Button onClick={toggleEditing}>Cancel</Button>
-                            </FormControl>
-                        </form>
-                    )}
-                </FormControl>
-            </Box>
-        </Box>
-      </div>
+    <ChakraProvider theme={customTheme}>
+      <CSSReset />
+      <Flex
+        h="100vh"
+        bgImage={backgroundImage}
+        bgSize="cover"
+        bgPosition="center"
+        color="white"
+      >
+        {/* Левая часть (30% экрана) */}
+        <Flex direction="column" align="center" justify="flex-start" w="30%" p={8}>
+          {/* Шапка профиля */}
+          <div className="Ava" style={{ width: 568, height: 268, position: 'relative' }}>
+            <div className="Rectangle177" style={{ width: 568, height: 185, left: 0, top: 0, position: 'absolute', background: 'white', borderTopLeftRadius: 19.55, borderTopRightRadius: 19.55, borderBottomRightRadius: 3, borderBottomLeftRadius: 3 }} />
+            <div className="Ellipse2" style={{ width: 213, height: 213, left: 178, top: 55, position: 'absolute', background: '#A5A5A5', borderRadius: 9999 }} />
+            <img className="Ellipse1" style={{ width: 205, height: 205, left: 182, top: 59, position: 'absolute', borderRadius: 9999 }} src={require('./Ellipse 1.png')} alt="Ellipse 1" />
+            <div className="Rectangle172" style={{ width: 97, height: 33, left: 181, top: 230, position: 'absolute', background: '#6EE544', borderRadius: 30 }} />
+            <div className="Online" style={{ width: 62, height: 29, left: 205, top: -6, position: 'absolute', color: '#090945', fontSize: 18, fontFamily: 'Poppins', fontWeight: '500', lineHeight: 28, fontStyle: "medium", wordWrap: 'break-word' }}>Online<br /></div>
+          </div>
+
+          {/* Другие элементы левой части */}
+        </Flex>
+
+        {/* Разделительная вертикальная линия */}
+        <div style={{ width: "2px", background: "white", height: "100%" }} />
+
+        {/* Правая часть (70% экрана) */}
+        <Flex direction="column" align="center" justify="center" w="70%">
+          <Text fontSize="lg" textAlign="center">
+            Hi, I'm John Doe, a passionate frontend developer with experience in building web applications.
+          </Text>
+          <Flex mt={4}>
+            <Button colorScheme="teal" mr={2}>
+              Follow
+            </Button>
+            <Button variant="outline" colorScheme="teal">
+              Message
+            </Button>
+          </Flex>
+          {/* Другие элементы правой части */}
+        </Flex>
+      </Flex>
     </ChakraProvider>
   );
-};
+}
 
-export default ProfilePage;
+export default App;
