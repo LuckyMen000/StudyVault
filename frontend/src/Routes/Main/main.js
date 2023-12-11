fb858367283d8350d8b8229f8de5690645506e34
LuckyMen000 <danil_unturned459@mail.ru>
2023-12-11 05:20:53 -0800

    #2

--- frontend/src/Routes/Main/main.js
+++ frontend/src/Routes/Main/main.js
@@ -12,10 +12,12 @@ const Main = () => {
     <ChakraProvider>
       <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '12px', padding: '5px', marginLeft:'10px'}}>
         <Sidebar />
-        <div>
-          <InputForm />
-          <div style={{ marginTop:'15px' }}>
-          <HomeWorkInfo/>            
+        <div style={{ position: 'absolute', marginLeft: '120px' }}>
+          <div>
+            <InputForm />
+            <div style={{ marginTop:'15px' }}>
+            <HomeWorkInfo/>            
+            </div>
           </div>
         </div>
         <div style={{ display: 'flex', flexDirection: 'column', }}>
