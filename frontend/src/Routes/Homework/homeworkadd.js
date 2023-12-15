d0846c46a87864394ad352af510c05cea0c58a1d
LuckyMen000 <danil_unturned459@mail.ru>
2023-12-14 21:39:46 -0800

    Homework comp

---
+++ frontend/src/Routes/Homework/homeworkadd.js
@@ -0,0 +1,29 @@
+import { Box, Input, Button, Heading, ChakraProvider } from '@chakra-ui/react';
+
+const HomeworkPage = () => {
+  const handlePublish = () => {
+    // Логика для публикации задания
+  };
+
+  return (
+    <Box maxW="md" mx="auto" mt={8} p={4}>
+      <Heading as="h1" mb={4}>
+        Создать домашнее задание
+      </Heading>
+      <Input placeholder="Введите задание..." mb={4} />
+      <Button colorScheme="blue" onClick={handlePublish}>
+        Опубликовать
+      </Button>
+    </Box>
+  );
+};
+
+const HomePage = () => {
+  return (
+    <ChakraProvider>
+      <HomeworkPage />
+    </ChakraProvider>
+  );
+};
+
+export default HomePage;
