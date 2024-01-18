396341f7c7a40d43aaf87a81789f665a3cb0cc9e
Ab-BeGa <abdulkhakimbegzat@gmail.com>
2024-01-18 16:31:26 +0600

    Исправил ошибку Контроллеров

--- backend/server/router/hometasks.js
+++ backend/server/router/hometasks.js
@@ -1,8 +1,6 @@
 const Router = require('express').Router
-const express = require('express')
 const router = new Router()
 const homeworks = require('../controllers/homeworks-controllers')
-const {json} = require("express");
 
 
 router.get('/',homeworks.getTasks)
